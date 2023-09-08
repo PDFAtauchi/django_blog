1100a00536f5d0ce5f31d6868dd8095e06f8d49b
PDFAtauchi <floresatauchi@gmail.com>
2023-09-08 14:56:30 -0500

    feat: implementation of Post model

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,9 +2,16 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+# Local application imports
+from apps.blog.models import Post
+
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all()
+    context = {
+        "posts": posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request: HttpRequest) -> HttpResponse:
     return render(request, 'blog/about.html')
---
+++ apps/tests/blog/test_models.py
@@ -0,0 +1,86 @@
+# Django framework
+from django.test import TestCase
+
+# Local application imports
+from apps.blog.models import Post
+from apps.tests.factories.users import UserFactory
+
+
+class TestPostModel(TestCase):
+    def setUp(self):
+        self.author = UserFactory()
+
+    def tearDown(self):
+        pass
+
+    def test_post_save(self):
+        # Given
+        post = Post(title="Blog Post 1", author_id=self.author.id)
+
+        # When
+        post.save()
+
+        # Then
+        self.assertEqual(post.title, "Blog Post 1")
+        self.assertEqual(post.__str__(), "Blog Post 1")
+
+    def test_post_retrieve(self):
+        # Given
+        post = Post(title="Blog Post 1", author_id=self.author.id)
+        post.save()
+
+        # When
+        post = Post.objects.filter(
+            pk=post.pk).first()
+
+        # Then
+        self.assertEqual(post.title, "Blog Post 1")
+
+    def test_post_update(self):
+        # Given
+        post = Post(title="Blog Post 1", author_id=self.author.id)
+        post.save()
+
+        # When
+        post = Post.objects.filter(
+            pk=post.pk).first()
+        self.assertEqual(post.title, "Blog Post 1")
+
+        # And
+        post.title = "Blog Post 2"
+        post.save()
+
+        # Then
+        self.assertEqual(post.title, "Blog Post 2")
+
+    def test_post_delete(self):
+        # Given
+        post = Post(title="Blog Post 1", author_id=self.author.id)
+        post.save()
+
+        # When
+        post = Post.objects.filter(
+            pk=post.pk).first()
+
+        # Then
+        self.assertIsNotNone(post)
+
+        # And, When
+        Post.objects.filter(pk=post.pk).delete()
+        post = Post.objects.filter(
+            pk=post.pk).first()
+
+        # Then
+        self.assertIsNone(post)
+
+    def test_post_list(self):
+        # Given
+        posts = ["Blog Post 1", "Blog Post 2"]
+
+        # When
+        for i, title in enumerate(posts):
+            Post(title=title, author_id=self.author.id).save()
+
+        # Then
+        posts_count = Post.objects.all().count()
+        self.assertEqual(posts_count, len(posts))
