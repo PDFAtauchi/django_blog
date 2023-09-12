62ed3e2c2968ca535bb4732d8d38921dd7c5cff0
PDFAtauchi <floresatauchi@gmail.com>
2023-09-12 14:32:41 -0500

    refactor: remove unused code

--- apps/tests/users/views/test_update_profile.py
+++ apps/tests/users/views/test_update_profile.py
@@ -64,24 +64,3 @@ class ProfileUpdateTest(TestCase):
         user_form = response.context["user_form"]
         assert response.status_code == 200
         assert user_form.errors["username"][0] == "Este campo es obligatorio."
-
-    # def test_profile_image_resize_test(self):
-    #     # Given
-    #     image = Image.new('RGB', (700, 700))
-    #     image_io = io.BytesIO()
-    #     image.save(image_io, 'JPEG')
-
-    #     self.client.login(username='testuser', password='testpassword')
-    #     image_file = SimpleUploadedFile("test_image.jpg", image_io.getvalue(), content_type="image/jpeg")
-
-    #     user_data = {'username': "test",
-    #             'email': "test@gmail.com"}
-    #     profile_data = {'image': image_file}
-    #     data = {**user_data, **profile_data}
-
-    #     # When
-    #     response = self.client.post(self.profile_account_url, data=data, format="json")
-
-    #     # Then
-    #     self.assertRedirects(response, self.profile_account_url, status_code=302,
-    #                        target_status_code=200)
