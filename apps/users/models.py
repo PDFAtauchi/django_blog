747d823c18e59f014242ebbb77c9370938a71aab
PDFAtauchi <floresatauchi@gmail.com>
2023-09-12 22:35:46 -0500

    refactor: Profile model implementation with tests

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,6 +2,9 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+# Third party imports
+from PIL import Image
+
 
 User = get_user_model()
 
@@ -9,5 +12,12 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.png', upload_to="profile_pics")
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
---
+++ apps/users/test_models.py
@@ -0,0 +1,40 @@
+# Standard library imports
+import io
+
+# Django framework
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+
+# Local application imports
+from apps.tests.factories.users import UserFactory
+from apps.users.models import Profile
+
+# Third party imports
+from PIL import Image
+
+
+class TestProfileModel(TestCase):
+    def setUp(self):
+        self.username = "Tom"
+        self.user = UserFactory(username=self.username)
+
+    def tearDown(self):
+        pass
+
+    def test_profile_resize_image_save(self):
+        # Given
+        image = Image.new('RGB', (700, 700))
+        image_io = io.BytesIO()
+        image.save(image_io, 'JPEG')
+        image_file = SimpleUploadedFile("test_image.jpg", image_io.getvalue(), content_type="image/jpeg")
+
+        profile = Profile.objects.get(user_id=self.user.id)
+        profile.image = image_file
+
+        # When
+        profile.save()
+
+        # Then
+        image_retrieved = Image.open(profile.image.path)
+        self.assertEqual(image_retrieved.height, 300)
+        self.assertEqual(image_retrieved.width, 300)
