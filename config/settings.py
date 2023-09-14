2ddca9d9d4be15cb210c8dee7e50c82834d8e5b6
Paul Dany Flores Atauchi <FloresAtauchi@gmail.com>
2023-09-13 22:12:26 -0500

    Reset password implementation with email (#14)

--- config/settings.py
+++ config/settings.py
@@ -166,3 +166,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = 'home-page'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env.str("EMAIL_USER", default="")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_PASS", default="")
